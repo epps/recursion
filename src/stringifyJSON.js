41581e69ee1a4fb81e97886f8ff9364ea0f3a14a
Erik Eppel <eceppel@gmail.com>
2015-06-09 12:02:27 -0700

    Implements stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,49 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // if (obj === null) {
+  // 	console.log("The obj " + obj + " is null");
+  // } else if (Array.isArray(obj)) {
+  // 	console.log("The obj " + obj + " is an array");
+  // } else {
+  // 	console.log("The obj " + obj + " is a " + typeof obj);
+  // }
+
+  if (obj === null) {
+  	return "null";
+  } else if (typeof obj === "string") {
+  	return '"'+obj+'"';
+
+  } else if (Array.isArray(obj)) {
+  	var strArr = '[',
+  		i = 0,
+  		leng = obj.length,
+  		lastEl = obj.length -1,
+  		el;
+  	for(i; i<leng; i++) {
+  		el = stringifyJSON(obj[i]);
+  		strArr += el;
+  		strArr += (i < lastEl) ? "," : "";
+  	}
+  	return strArr += ']';
+  } else if (typeof obj === "object") {
+  	var strObj = '{',
+  		keyStr,
+  		valStr,
+  		lastkey = Object.keys(obj).pop(); 
+  	for (var key in obj) {
+  		if (typeof obj[key] === "function" || typeof obj[key] === "undefined") {
+  			strObj += "";
+  		} else {
+  		keyStr = stringifyJSON(key);
+  		valStr = stringifyJSON(obj[key]);
+  		strObj += keyStr + ':' + valStr;
+  		strObj += (key == lastkey) ? "" : ",";
+	  	}
+  	}
+  	return strObj += '}';
+  } else {
+  	return obj.toString();
+  }
+
 };
